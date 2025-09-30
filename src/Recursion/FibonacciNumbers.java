e88ef6cef52480a87157150818c8b049b8ba55b7
Rocco <popescu.rocco@yahoo.com>
2025-09-30 10:59:37 +0300

    FibonacciNumbers.java - DONE

--- src/Recursion/FibonacciNumbers.java
+++ src/Recursion/FibonacciNumbers.java
@@ -1,4 +1,34 @@
 package Recursion;
 
 public class FibonacciNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(fib(i));
+        }
+
+    }
+
+
+    /*
+    necesita optimizare pentru ca aceasta functie e mai mult un brute force
+    care nu poate sa arate pentru un n mic ca de ex fib(50) deoarece face apelari
+    la functie care se tot repeta
+        fib(50) = fib(49) + fib(48)
+        fib(49) = fib(48) + fib(47)
+        -> se repeta fiecare ramura a arborelui recursiv de ex fib(48) si tot asa
+     */
+    public static int fib(int num) {
+        if(num == 0 || num == 1) {
+            return num;
+        }
+
+        return fib(num-1) + fib(num-2);
+
+    }
+
+
+
+
 }
