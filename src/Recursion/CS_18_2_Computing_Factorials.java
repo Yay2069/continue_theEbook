29aeca172949aeed14ffddd2fe8d09868bdb94c5
Rocco <popescu.rocco@yahoo.com>
2025-10-05 13:33:52 +0300

    CS_18_2_Computing_Factorials - DONE

--- src/Recursion/CS_18_2_Computing_Factorials.java
+++ src/Recursion/CS_18_2_Computing_Factorials.java
@@ -4,8 +4,16 @@ public class CS_18_2_Computing_Factorials {
 
     public static void main(String[] args) {
 
+        System.out.println(factiorial(5));
 
 
+    }
+
+    public static long factiorial(int n) {
+
+        if(n == 0) return 1;
+
+        return n * factiorial(n-1);
     }
 
 
