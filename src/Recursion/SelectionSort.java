791cf21cb3b954cd0ea3b10758d3fc1e5741efed
Rocco <popescu.rocco@yahoo.com>
2025-10-21 15:59:25 +0300

    Added SelectionSort

---
+++ src/Recursion/SelectionSort.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 7, 4, 1, 5};
+
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+    //iterative selection sort
+    public static void selectionSort(int[] arr) {
+
+
+
+    }
+
+
+
+    //recursive selection sort
+
+
+}
